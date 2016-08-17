620c49b9fb6644243c3544fea48ef858c5dd38ff
Sock <sock@andrewlock.net>
2016-08-17 08:57:35 +0100

    v3.1.6 - Update prism-theme

--- assets/js/src/__init.js
+++ assets/js/src/__init.js
@@ -5,7 +5,7 @@ $(function() {
         openHash = "#open";
 
     window.CaffeineTheme = CaffeineTheme = {
-        version: "3.1.5",
+        version: "3.1.6",
         search: {
             container: function() {
                 return $("#results");
