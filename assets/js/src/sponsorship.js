12681f26c02ba2c62fb8dbfa9449799ce0288f4d
Sock <sock@andrewlock.net>
2016-11-10 19:52:30 +0000

    Add outbound link tracking to google analytics

--- assets/js/src/sponsorship.js
+++ assets/js/src/sponsorship.js
@@ -6,20 +6,39 @@ $(function () {
             $list = $('.posts');
 
         if ($post.length) {
-            var postTemplate = $('<div class="sponsor"><p><a href="'
+            var postTemplate = '<div class="sponsor"><p><a href="'
                 + sponsorUrl
-                + '" target="_blank"><strong>Sponsored&nbsp;By: </strong><span>'
+                + '" target="_blank"';
+
+            //only add outbound link tracking if we are using google analytics
+            //see https://support.google.com/analytics/answer/1136920?hl=en
+            if (window.ga_id) {
+                postTemplate += ' onclick="window.trackOutLink(\'' + sponsorUrl + '\');"'
+            }
+
+            postTemplate +=
+                '><strong>Sponsored&nbsp;By: </strong><span>'
                 + sponsorHtml
-                + '</span>&nbsp;<i class="fa fa-external-link"></i></a></p></div>');
+                + '</span>&nbsp;<i class="fa fa-external-link"></i></a></p></div>';
+
             $post.prepend(postTemplate);
         }
 
         if ($list.length) {
-            var listTemplate = $('<li class="card sponsor"><div class="card-container"><div class="card-body"><a href="'
+            var listTemplate = '<li class="card sponsor"><div class="card-container"><div class="card-body"><a href="'
                 + sponsorUrl
-                + '" target="_blank"><div class="card-content"><strong>Sponsored&nbsp;By: </strong><span>'
+                + '" target="_blank"';
+
+            //only add outbound link tracking if we are using google analytics
+            //see https://support.google.com/analytics/answer/1136920?hl=en
+            if (window.ga_id) {
+                listTemplate += ' onclick="window.trackOutLink(\'' + sponsorUrl + '\');"'
+            }
+
+            listTemplate +=
+                '><div class="card-content"><strong>Sponsored&nbsp;By: </strong><span>'
                 + sponsorHtml
-                + '</span></div></a></div></div></li>');
+                + '</span></div></a></div></div></li>';
             $list.prepend(listTemplate);
         }
     }
